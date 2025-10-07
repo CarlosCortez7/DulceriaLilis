efec7f905946f0a8689c58108fe5f7ee7f1e170c
kusitoO <200vloneh@gmail.com>
2025-10-06 21:49:24 -0300

    Avances en Admin Pro: Inline funcional en categoria para agregar mas productos

--- productos/admin.py
+++ productos/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import Categoria, Producto
 
+# Definir el modelo inline para Producto
+class ProductoInline(admin.TabularInline):
+    model = Producto
+    extra = 1  
+    fields = ('id_interno', 'nombre', 'precio_venta', 'unidad_compra', 'unidad_venta')
+    show_change_link = True  # Para mostrar el enlace a los productos existentes
+
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre')
     search_fields = ('nombre',)
+    inlines = [ProductoInline]  # Aquí se pasa la clase, no el string
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
