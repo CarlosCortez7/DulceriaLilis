b722e5b861933d1af46f62ed8bf1b930b64f145d
Carlos Cortez <carlos.cortez20@inacapmail.cl>
2025-10-17 00:05:00 -0300

    actualizacion database settings

--- core/settings.py
+++ core/settings.py
@@ -84,9 +84,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DB_MODE = os.getenv("DB_MODE", "sqlite")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
