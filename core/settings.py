e1661bba2a8f85bc318f73d8726d19b924ee099f
Carlos Cortez <carlos.cortez20@inacapmail.cl>
2025-11-23 21:59:52 -0300

    404 html

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
